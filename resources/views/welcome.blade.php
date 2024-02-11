d0061b0db8bc407357181ac529789843a9d1f125
Gading Khairlambang <sasakihaise985@gmail.com>
2024-02-11 19:10:41 +0700

    web himpunan

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Himpunan Web</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    </head>
+    <body class="antialiased">
+        <nav class="border-b-4  p-4">
+            <div class="container mx-auto flex justify-around items-center">
+                <!-- Logo -->
+                <a href="#" class="text-stale-800 transition duration:300 text-lg font-bold">HIMATIF</a>
+                    <!-- Navigation Links -->
+                <div class="space-x-5">
+                    <a href="#" class="text-stale-800 hover:opacity-50 transition duration:300">Home</a>
+                    <a href="#" class="text-stale-800 hover:opacity-50 transition duration:300">Visi dan Misi</a>
+                    <a href="#" class="text-stale-800 hover:opacity-50 transition duration:300">Struktur Organisasi</a>
+                    <a href="#" class="text-stale-800 hover:opacity-50 transition duration:300">Divisi</a>
+                    <a href="#" class="text-stale-800 hover:opacity-50 transition duration:300">open recruitment</a>
+                </div>
+            </div>
+        </nav>
+    </body>
+</html>
