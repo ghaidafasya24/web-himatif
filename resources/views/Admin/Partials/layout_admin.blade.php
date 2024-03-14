bbb7dd0ad949dbebd73a4fbe1904bd9b281ec945
Gading Khairlambang <sasakihaise985@gmail.com>
2024-03-14 19:58:31 +0700

    fix navbar and sidebar

--- resources/views/Admin/Partials/layout_admin.blade.php
+++ resources/views/Admin/Partials/layout_admin.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
     <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
@@ -21,8 +22,220 @@
     @yield('content')
     @include('Admin.Partials.navbar_admin')
     @include('Admin.Partials.sidebar_admin')
-   
 
+    <script>
+        // start: Sidebar
+        const sidebarToggle = document.querySelector('.sidebar-toggle')
+        const sidebarOverlay = document.querySelector('.sidebar-overlay')
+        const sidebarMenu = document.querySelector('.sidebar-menu')
+        const main = document.querySelector('.main')
+        if (window.innerWidth < 768) {
+            main.classList.toggle('active')
+            sidebarOverlay.classList.toggle('hidden')
+            sidebarMenu.classList.toggle('-translate-x-full')
+        }
+        sidebarToggle.addEventListener('click', function(e) {
+            e.preventDefault()
+            main.classList.toggle('active')
+            sidebarOverlay.classList.toggle('hidden')
+            sidebarMenu.classList.toggle('-translate-x-full')
+        })
+        sidebarOverlay.addEventListener('click', function(e) {
+            e.preventDefault()
+            main.classList.add('active')
+            sidebarOverlay.classList.add('hidden')
+            sidebarMenu.classList.add('-translate-x-full')
+        })
+        document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function(item) {
+            item.addEventListener('click', function(e) {
+                e.preventDefault()
+                const parent = item.closest('.group')
+                if (parent.classList.contains('selected')) {
+                    parent.classList.remove('selected')
+                } else {
+                    document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function(i) {
+                        i.closest('.group').classList.remove('selected')
+                    })
+                    parent.classList.add('selected')
+                }
+            })
+        })
+        // end: Sidebar
+
+
+
+        // start: Popper
+        const popperInstance = {}
+        document.querySelectorAll('.dropdown').forEach(function(item, index) {
+            const popperId = 'popper-' + index
+            const toggle = item.querySelector('.dropdown-toggle')
+            const menu = item.querySelector('.dropdown-menu')
+            menu.dataset.popperId = popperId
+            popperInstance[popperId] = Popper.createPopper(toggle, menu, {
+                modifiers: [{
+                        name: 'offset',
+                        options: {
+                            offset: [0, 8],
+                        },
+                    },
+                    {
+                        name: 'preventOverflow',
+                        options: {
+                            padding: 24,
+                        },
+                    },
+                ],
+                placement: 'bottom-end'
+            });
+        })
+        document.addEventListener('click', function(e) {
+            const toggle = e.target.closest('.dropdown-toggle')
+            const menu = e.target.closest('.dropdown-menu')
+            if (toggle) {
+                const menuEl = toggle.closest('.dropdown').querySelector('.dropdown-menu')
+                const popperId = menuEl.dataset.popperId
+                if (menuEl.classList.contains('hidden')) {
+                    hideDropdown()
+                    menuEl.classList.remove('hidden')
+                    showPopper(popperId)
+                } else {
+                    menuEl.classList.add('hidden')
+                    hidePopper(popperId)
+                }
+            } else if (!menu) {
+                hideDropdown()
+            }
+        })
+
+        function hideDropdown() {
+            document.querySelectorAll('.dropdown-menu').forEach(function(item) {
+                item.classList.add('hidden')
+            })
+        }
+
+        function showPopper(popperId) {
+            popperInstance[popperId].setOptions(function(options) {
+                return {
+                    ...options,
+                    modifiers: [
+                        ...options.modifiers,
+                        {
+                            name: 'eventListeners',
+                            enabled: true
+                        },
+                    ],
+                }
+            });
+            popperInstance[popperId].update();
+        }
+
+        function hidePopper(popperId) {
+            popperInstance[popperId].setOptions(function(options) {
+                return {
+                    ...options,
+                    modifiers: [
+                        ...options.modifiers,
+                        {
+                            name: 'eventListeners',
+                            enabled: false
+                        },
+                    ],
+                }
+            });
+        }
+        // end: Popper
+
+
+
+        // start: Tab
+        document.querySelectorAll('[data-tab]').forEach(function(item) {
+            item.addEventListener('click', function(e) {
+                e.preventDefault()
+                const tab = item.dataset.tab
+                const page = item.dataset.tabPage
+                const target = document.querySelector('[data-tab-for="' + tab + '"][data-page="' + page + '"]')
+                document.querySelectorAll('[data-tab="' + tab + '"]').forEach(function(i) {
+                    i.classList.remove('active')
+                })
+                document.querySelectorAll('[data-tab-for="' + tab + '"]').forEach(function(i) {
+                    i.classList.add('hidden')
+                })
+                item.classList.add('active')
+                target.classList.remove('hidden')
+            })
+        })
+        // end: Tab
+
+
+
+        // start: Chart
+        new Chart(document.getElementById('order-chart'), {
+            type: 'line',
+            data: {
+                labels: generateNDays(7),
+                datasets: [{
+                        label: 'Active',
+                        data: generateRandomData(7),
+                        borderWidth: 1,
+                        fill: true,
+                        pointBackgroundColor: 'rgb(59, 130, 246)',
+                        borderColor: 'rgb(59, 130, 246)',
+                        backgroundColor: 'rgb(59 130 246 / .05)',
+                        tension: .2
+                    },
+                    {
+                        label: 'Completed',
+                        data: generateRandomData(7),
+                        borderWidth: 1,
+                        fill: true,
+                        pointBackgroundColor: 'rgb(16, 185, 129)',
+                        borderColor: 'rgb(16, 185, 129)',
+                        backgroundColor: 'rgb(16 185 129 / .05)',
+                        tension: .2
+                    },
+                    {
+                        label: 'Canceled',
+                        data: generateRandomData(7),
+                        borderWidth: 1,
+                        fill: true,
+                        pointBackgroundColor: 'rgb(244, 63, 94)',
+                        borderColor: 'rgb(244, 63, 94)',
+                        backgroundColor: 'rgb(244 63 94 / .05)',
+                        tension: .2
+                    },
+                ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        function generateNDays(n) {
+            const data = []
+            for (let i = 0; i < n; i++) {
+                const date = new Date()
+                date.setDate(date.getDate() - i)
+                data.push(date.toLocaleString('en-US', {
+                    month: 'short',
+                    day: 'numeric'
+                }))
+            }
+            return data
+        }
+
+        function generateRandomData(n) {
+            const data = []
+            for (let i = 0; i < n; i++) {
+                data.push(Math.round(Math.random() * 10))
+            }
+            return data
+        }
+        // end: Chart
+    </script>
 </body>
 
 </html>
