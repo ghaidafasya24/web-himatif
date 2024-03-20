be87a4f482667885437b12431133b2979a96a23c
Gading Khairlambang <sasakihaise985@gmail.com>
2024-03-20 16:33:53 +0700

    add news dashboard

--- resources/views/Admin/News/event.blade.php
+++ resources/views/Admin/News/event.blade.php
@@ -3,72 +3,56 @@
 @section('content')
 
 <div class="overflow-x-hidden md:w-[calc(100%-256px)] md:ml-64 mt-16 w-full">
-    <div class="flex justify-end mr-10">
-        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Add Berita</button>
+    <div class="block ml-10">
+        <div class="flex">
+            <i class="fa-regular fa-folder mr-3 text-lg"></i>
+            <p>Data Event</p>
+        </div>
+        <button class="mt-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"><i class="fa-solid fa-plus mr-2"></i>Add Berita</button>
     </div>
-    <table class="table-auto min-w-full divide-y divide-gray-200 overflow-x-hidden">
-        <thead class="bg-gray-50">
-            <tr>
-                <th scope="col" class="px-10 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Judul Berita</th>
-                <th scope="col" class="px-10 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tanggal Terbit</th>
-                <th scope="col" class="px-10 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Kategori</th>
-                <th scope="col" class="px-10 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Penulis</th>
-                <th scope="col" class="px-10 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Aksi</th>
-            </tr>
-        </thead>
-        <tbody class="bg-white divide-y divide-gray-200">
-            <tr>
-                <td class="px-10 py-2 whitespace-nowrap">Judul Berita 1</td>
-                <td class="px-10 py-2 whitespace-nowrap">12 Maret 2024</td>
-                <td class="px-10 py-2 whitespace-nowrap">Teknologi</td>
-                <td class="px-10 py-2 whitespace-nowrap">John Doe</td>
-                <td class="px-10 py-2 whitespace-nowrap">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
-                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
-                </td>
-            </tr>
-            <tr>
-                <td class="px-10 py-2 whitespace-nowrap">Judul Berita 1</td>
-                <td class="px-10 py-2 whitespace-nowrap">12 Maret 2024</td>
-                <td class="px-10 py-2 whitespace-nowrap">Teknologi</td>
-                <td class="px-10 py-2 whitespace-nowrap">John Doe</td>
-                <td class="px-10 py-2 whitespace-nowrap">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
-                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
-                </td>
-            </tr>
-            <tr>
-                <td class="px-10 py-2 whitespace-nowrap">Judul Berita 1</td>
-                <td class="px-10 py-2 whitespace-nowrap">12 Maret 2024</td>
-                <td class="px-10 py-2 whitespace-nowrap">Teknologi</td>
-                <td class="px-10 py-2 whitespace-nowrap">John Doe</td>
-                <td class="px-10 py-2 whitespace-nowrap">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
-                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
-                </td>
-            </tr>
-            <tr>
-                <td class="px-10 py-2 whitespace-nowrap">Judul Berita 1</td>
-                <td class="px-10 py-2 whitespace-nowrap">12 Maret 2024</td>
-                <td class="px-10 py-2 whitespace-nowrap">Teknologi</td>
-                <td class="px-10 py-2 whitespace-nowrap">John Doe</td>
-                <td class="px-10 py-2 whitespace-nowrap">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
-                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
-                </td>
-            </tr>
-            <tr>
-                <td class="px-10 py-2 whitespace-nowrap">Judul Berita 1</td>
-                <td class="px-10 py-2 whitespace-nowrap">12 Maret 2024</td>
-                <td class="px-10 py-2 whitespace-nowrap">Teknologi</td>
-                <td class="px-10 py-2 whitespace-nowrap">John Doe</td>
-                <td class="px-10 py-2 whitespace-nowrap">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
-                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="ml-4 mr-4">
+        <div class="grid grid-cols-3 gap-4">
+            <div class="p-4 bg-white rounded-lg shadow-xl flex">
+                <img class="mr-2 fa-solid fa-comment text-gray-500 dark:text-gray-400 text-4xl my-4 rounded-md" src="https://source.unsplash.com/random/120x120?" alt="berita"></img>
+                <div class="flex flex-col">
+                    <h2 class="text-xl font-bold mt-4">Berita Acara Title</h2>
+                    <p>Tanggal</p>
+                    <p>Deskripsi</p>
+                    <div class="mt-2 flex justify-evenly gap-4">
+                        <a href="" class="bg-blue-500 text-white font-semibold py-1 px-3 border border-blue-500 rounded-lg shadow">Detail</a>
+                        <a href="" class="bg-yellow-500 text-white font-semibold py-1 px-3 border border-yellow-500 rounded-lg shadow">Edit</a>
+                        <a href="" class="bg-red-500 text-white font-semibold py-1 px-3 border border-red-500 rounded-lg shadow">Delete</a>
+                    </div>
+                </div>
+            </div>
+            <div class="p-4 bg-white rounded-lg shadow-xl flex">
+                <img class="mr-2 fa-solid fa-comment text-gray-500 dark:text-gray-400 text-4xl my-4 rounded-md" src="https://source.unsplash.com/random/120x120?" alt="berita"></img>
+                <div class="flex flex-col">
+                    <h2 class="text-xl font-bold mt-4">Berita Acara Title</h2>
+                    <p>Tanggal</p>
+                    <p>Deskripsi</p>
+                    <div class="mt-2 flex justify-evenly gap-4">
+                        <a href="" class="bg-blue-500 text-white font-semibold py-1 px-3 border border-blue-500 rounded-lg shadow">Detail</a>
+                        <a href="" class="bg-yellow-500 text-white font-semibold py-1 px-3 border border-yellow-500 rounded-lg shadow">Edit</a>
+                        <a href="" class="bg-red-500 text-white font-semibold py-1 px-3 border border-red-500 rounded-lg shadow">Delete</a>
+                    </div>
+                </div>
+            </div>
+            <div class="p-4 bg-white rounded-lg shadow-xl flex">
+                <img class="mr-2 fa-solid fa-comment text-gray-500 dark:text-gray-400 text-4xl my-4 rounded-md" src="https://source.unsplash.com/random/120x120?" alt="berita"></img>
+                <div class="flex flex-col">
+                    <h2 class="text-xl font-bold mt-4">Berita Acara Title</h2>
+                    <p>Tanggal</p>
+                    <p>Deskripsi</p>
+                    <div class="mt-2 flex justify-evenly gap-4">
+                        <a href="" class="bg-blue-500 text-white font-semibold py-1 px-3 border border-blue-500 rounded-lg shadow">Detail</a>
+                        <a href="" class="bg-yellow-500 text-white font-semibold py-1 px-3 border border-yellow-500 rounded-lg shadow">Edit</a>
+                        <a href="" class="bg-red-500 text-white font-semibold py-1 px-3 border border-red-500 rounded-lg shadow">Delete</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
