ee328e9592a31882cb407a7eb809be0b7d66ccb0
FARAZIFTAN <mahasiswaulbiit@gmail.com>
2024-03-22 09:27:08 +0700

    FARAZIFTAN

--- resources/views/AboutUs/abousus.blade.php
+++ resources/views/AboutUs/abousus.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
-<body class="bg-orange-100">
+<body class="bg-orange-200">
     
         
   <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl"></div>
