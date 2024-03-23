2bc84d7edd9112ec37a1267dc16e675db5eb8ca4
Gading Khairlambang <sasakihaise985@gmail.com>
2024-03-23 18:05:43 +0700

    fixed error

--- resources/views/Admin/Docs/Gallery/edit.blade.php
+++ resources/views/Admin/Docs/Gallery/edit.blade.php
@@ -12,44 +12,6 @@
                 <p>Edit Gallery</p>
             </div>
         </div>
-<<<<<<< HEAD
-    </div>
-    <div class="mx-10 mt-10">
-        <div class="relative w-full">
-            <label class="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" for="grid-password">
-                Judul Gallery
-            </label>
-            <input class="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-        </div>
-        <div class="relative w-full">
-            <label class="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" for="deskripsi">
-                Deskripsi
-            </label>
-            <textarea id="deskripsi" class="resize-none appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"></textarea>
-        </div>
-        <div class="relative w-full">
-            <label class="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" for="grid-password">
-                Tanggal Publish
-            </label>
-            <input type="date" class="hb-32 appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-        </div>
-        <div class="relative w-full">
-            <label class="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" for="grid-password">
-                Gambar
-            </label>
-            <input class="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="file_input" type="file">
-        </div>
-        <div class="w-full bg-gray-200 mt-36">
-            <button class="mr-6 bg-blue-500 hover:opacity-80 border-transparant text-white font-semibold py-2 px-10 transition rounded shadow">
-                Back
-            </button>
-            <button class="bg-yellow-400 hover:opacity-80 border-transparant font-semibold py-2 px-10 transition rounded shadow">
-                Edit
-            </button>
-        </div>
-    </div>
-</div>
-=======
         <div class="mx-10 mt-10">
             <div class="relative w-full">
                 <label class="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" for="grid-password">
@@ -58,8 +20,6 @@
                 <input name="judul_gallery" value="{{ $detail->judul_gallery }}"
                     class="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
             </div>
->>>>>>> cdb706be270d904e77f100b9995c78a64083b334
-
             <div class="relative w-full">
                 <label class="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" for="deskripsi">
                     Deskripsi
--- resources/views/Admin/Docs/Gallery/tambah.blade.php
+++ resources/views/Admin/Docs/Gallery/tambah.blade.php
@@ -52,37 +52,6 @@
                 </button>
             </div>
         </div>
-<<<<<<< HEAD
-        <div class="relative w-full">
-            <label class="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" for="deskripsi">
-                Deskripsi
-            </label>
-            <textarea id="deskripsi" class="resize-none appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"></textarea>
-        </div>
-        <div class="relative w-full">
-            <label class="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" for="grid-password">
-                Tanggal Publish
-            </label>
-            <input type="date" class="hb-32 appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-        </div>
-        <div class="relative w-full">
-            <label class="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" for="grid-password">
-                Gambar
-            </label>
-            <input class="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="file_input" type="file">
-        </div>
-        <div class="w-full bg-gray-200 mt-36">
-            <button class="mr-6 bg-blue-500 hover:opacity-80 border-transparant text-white font-semibold py-2 px-10 transition rounded shadow">
-                Back
-            </button>
-            <button class="bg-yellow-400 hover:opacity-80 border-transparant font-semibold py-2 px-10 transition rounded shadow">
-                Edit
-            </button>
-        </div>
-    </div>
-</div>
-=======
     </form>
->>>>>>> cdb706be270d904e77f100b9995c78a64083b334
 
 @endsection
